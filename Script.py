c8a297ed412d3d8969ada9f28c731ba7c9db3d03
muhammadali286 <muhammad.ali@arbisoft.com>
2020-07-25 00:53:15 +0500

    Added logs

--- Script.py
+++ Script.py
@@ -119,7 +119,7 @@ def chromeInit():
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-popup-blocking");
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     # chrome_options.add_argument("start-maximized")
     chrome_options.add_argument('--disable-dev-shm-usage')
